7c04b6b77f31153f5e813c381099b6abf9b07de5
Murilo Augusto <mu10-2015@hotmail.com>
2025-10-29 11:48:40 -0300

    feat: integrate fastify swagger and scalar API reference for development

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,4 +1,12 @@
 import fastify from "fastify";
+import fastifySwagger from "@fastify/swagger";
+import {
+	validatorCompiler,
+	serializerCompiler,
+	jsonSchemaTransform,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import scalarAPIReference from "@scalar/fastify-api-reference";
 
 export const app = fastify({
 	logger: {
@@ -10,4 +18,27 @@ export const app = fastify({
 			},
 		},
 	},
-});
+}).withTypeProvider<ZodTypeProvider>();
+
+if (process.env.NODE_ENV === "development") {
+	app.register(fastifySwagger, {
+		openapi: {
+			info: {
+				title: "IronKey",
+				description: "IronKey API.",
+				version: "0.1.0",
+			},
+		},
+		transform: jsonSchemaTransform,
+	});
+
+	app.register(scalarAPIReference, {
+		routePrefix: "/docs",
+		configuration: {
+			theme: "elysiajs",
+		},
+	});
+}
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
