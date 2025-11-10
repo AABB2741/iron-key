4e32aa03bb9f51439847bc7dc924580e52d78f66
AABB2741 <mariosantos88477364484@gmail.com>
2025-11-10 15:49:07 -0300

    ci: configure cd for backend

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,63 +1,64 @@
-import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
-import {
-	validatorCompiler,
-	serializerCompiler,
-	jsonSchemaTransform,
-	type ZodTypeProvider,
-} from "fastify-type-provider-zod";
 import scalarAPIReference from "@scalar/fastify-api-reference";
+import fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-import { env } from "./env.ts";
 import fastifyCors from "@fastify/cors";
+import { env } from "./env.ts";
 import { errorHandler } from "./error-handler.ts";
-import { signUpRoute } from "./routes/sign-up.ts";
 import { signInRoute } from "./routes/sign-in.ts";
+import { signUpRoute } from "./routes/sign-up.ts";
 
 export const app = fastify({
-	logger: {
-		level: env.LOGGER,
-		transport: {
-			target: "pino-pretty",
-			options: {
-				translateTime: "HH:MMss Z",
-				ignore: "pid,hostname",
-			},
-		},
-	},
+  logger: {
+    level: env.LOGGER,
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MMss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
 }).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(fastifyCors, {
-	origin: env.WEB_URL,
-	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  origin: env.WEB_URL,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 });
 
 if (env.NODE_ENV === "development") {
-	app.register(fastifySwagger, {
-		openapi: {
-			info: {
-				title: "IronKey",
-				description: "IronKey API.",
-				version: "0.1.0",
-			},
-		},
-		transform: jsonSchemaTransform,
-	});
+  app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "IronKey",
+        description: "IronKey API.",
+        version: "0.1.0",
+      },
+    },
+    transform: jsonSchemaTransform,
+  });
 
-	app.register(scalarAPIReference, {
-		routePrefix: "/docs",
-		configuration: {
-			theme: "elysiajs",
-		},
-	});
+  app.register(scalarAPIReference, {
+    routePrefix: "/docs",
+    configuration: {
+      theme: "elysiajs",
+    },
+  });
 }
 
 app.setErrorHandler(errorHandler);
 
 app.get("/", () => "Ok");
+app.get("/test", () => "Hello world!");
 
 app.register(signUpRoute);
 app.register(signInRoute);
