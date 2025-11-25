c628dfe2ab1c118c0b7e2c9c09e2e8a684c1ca91
AABB2741 <mariosantos88477364484@gmail.com>
2025-11-24 21:34:10 -0300

    feat(infra): create identity with access to storage account

--- infra/apps/desktop/storage-account.tf
+++ infra/apps/desktop/storage-account.tf
@@ -17,3 +17,20 @@ resource "azurerm_storage_container" "desktop_storage_container" {
   storage_account_id    = azurerm_storage_account.desktop_storage_account.id
   container_access_type = "blob"
 }
+
+resource "azurerm_user_assigned_identity" "desktop_storage_identity" {
+  name                = "${var.project_name}-desktop-storage-identity"
+  resource_group_name = var.resource_group_name
+  location            = data.azurerm_resource_group.rg.location
+
+  tags = {
+    Project = var.project_name
+    IAC     = true
+  }
+}
+
+resource "azurerm_role_assignment" "desktop_storage_role_assignment" {
+  scope                = azurerm_storage_account.desktop_storage_account.id
+  principal_id         = azurerm_user_assigned_identity.desktop_storage_identity.principal_id
+  role_definition_name = "Storage Blob Data Contributor"
+}
