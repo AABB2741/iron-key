8d99be7a92a68e2be106c1b673f925c36a61d556
AABB2741 <mariosantos88477364484@gmail.com>
2025-11-26 19:38:56 -0300

    feat: add responsive design for download page

--- apps/desktop/src/app/download.tsx
+++ apps/desktop/src/app/download.tsx
@@ -42,14 +42,16 @@ function RouteComponent() {
 
       <Layout.Container className="space-y-24 p-12 mx-auto">
         <section className="text-center space-y-6">
-          <div>Totalmente gratuito</div>
+          <p className="bg-success/50 rounded-full px-3 py-2 mx-auto w-fit text-sm">
+            Totalmente gratuito
+          </p>
           <h1 className="text-5xl font-bold">Gerador de senhas desktop</h1>
           <p>
             Gere senhas fortes offline no seu computador.
             <br />
             Login opcional para sincronização em nuvem.
           </p>
-          <div className="flex justify-center gap-6">
+          <div className="flex sm:flex-row flex-col justify-center gap-6">
             <Button variant="primary">
               <DownloadIcon />
               <span>Baixar para Windows</span>
@@ -67,7 +69,7 @@ function RouteComponent() {
         </section>
         <section className="text-center space-y-6">
           <h2 className="font-bold text-3xl">Por que usar o IronKey?</h2>
-          <div className="flex gap-6 items-start justify-center">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 items-start justify-center">
             <div className="border rounded-xl border-border p-4 text-left space-y-4">
               <ShieldIcon className="text-muted-foreground size-12" />
               <h3 className="font-bold text-lg">Completamente gratuito</h3>
@@ -93,8 +95,8 @@ function RouteComponent() {
         <section className="text-center space-y-6">
           <h2 className="font-bold text-3xl">Escolha sua plataforma</h2>
           <p>Disponível para Windows, macOS e Linux</p>
-          <div className="flex items-center gap-6 justify-center">
-            <div className="border border-border rounded-xl p-4 space-y-4 flex-1">
+          <div className="grid grid-cols-1 md:grid-cols-3 items-center gap-6 justify-center">
+            <div className="border border-border rounded-xl p-4 space-y-4">
               <h3 className="font-bold text-xl">Windows</h3>
               <div>
                 <p>v1.0.0</p>
@@ -105,7 +107,7 @@ function RouteComponent() {
                 <span>Baixar</span>
               </Button>
             </div>
-            <div className="border border-border rounded-xl p-4 space-y-4 flex-1">
+            <div className="border border-border rounded-xl p-4 space-y-4">
               <h3 className="font-bold text-xl">MacOS</h3>
               <div>
                 <p>v1.0.0</p>
@@ -116,7 +118,7 @@ function RouteComponent() {
                 <span>Baixar</span>
               </Button>
             </div>
-            <div className="border border-border rounded-xl p-4 space-y-4 flex-1">
+            <div className="border border-border rounded-xl p-4 space-y-4">
               <h3 className="font-bold text-xl">Linux</h3>
               <div>
                 <p>v1.0.0</p>
@@ -132,34 +134,34 @@ function RouteComponent() {
 
         <section className="text-center space-y-6">
           <h2 className="font-bold text-3xl">Recursos incluídos</h2>
-          <div className="grid grid-cols-2 mx-auto max-w-[750px] gap-x-8 gap-y-6">
-            <div className="flex items-center gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 mx-auto max-w-[750px] gap-x-8 gap-y-6">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Geração de senhas com até 64 caracteres</span>
+              <span>Geração de senhas com até 64 caracteres</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Interface moderna e intuitiva</span>
+              <span>Interface moderna e intuitiva</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Multiplataforma (Windows, Mac, Linux)</span>
+              <span>Multiplataforma (Windows, Mac, Linux)</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Totalmente gratuito, sem anúncios</span>
+              <span>Totalmente gratuito, sem anúncios</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Personalização completa de senhas</span>
+              <span>Personalização completa de senhas</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Login opcional para sincronização em nuvem</span>
+              <span>Login opcional para sincronização em nuvem</span>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 text-left">
               <CheckIcon className="text-success" />
-              <span> Código aberto e auditável</span>
+              <span>Código aberto e auditável</span>
             </div>
           </div>
         </section>
